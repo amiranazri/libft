7ae39cf71089bfc3c9c66fa51c1b8213bd91fcb9
Amira Nazri <anazri@hotmail.com>
2019-06-03 15:05:10 +0200

    Add files via upload

---
+++ ft_itoa.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anazri <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/03 14:38:40 by anazri            #+#    #+#             */
+/*   Updated: 2019/06/03 15:02:56 by anazri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	unsigned int		size;
+	unsigned int		con;
+	unsigned int		sign;
+
+	size = 1;
+	sign = 0;
+	if (n < 0)
+		sign = n * -1;
+	con = n;
+	while (con /= 10)
+		i++;
+	str = ft_strnew(size + sign);
+	if (str == NULL)
+		return (NULL);
+
+}
