5beff75e952f97fb4348fb54252eb4d44c0dd40f
Nazri AMIRA <anazri@c4r18s2.wethinkcode.co.za>
2019-06-13 14:47:01 +0200

    Final Norminette

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,56 +1,54 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi copy.c                                     :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anazri <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/20 08:23:38 by anazri            #+#    #+#             */
-/*   Updated: 2019/06/11 18:33:10 by anazri           ###   ########.fr       */
+/*   Created: 2019/06/13 09:21:40 by anazri            #+#    #+#             */
+/*   Updated: 2019/06/13 09:24:39 by anazri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_bool  is_digit(int c)
+int	ft_is_digit(int c)
 {
-    if (c >= 48 && c <= 57)
-        return (true);
+	if (c >= 48 && c <= 57)
+		return (1);
 	else
-		return (false);
+		return (0);
 }
 
-t_bool  is_space(int c)
+int	is_space(int c)
 {
-    if ((c >= 9 && c <= 13) || c == 32)
-        return (true);
+	if ((c >= 9 && c <= 13) || c == 32)
+		return (1);
 	else
-		return (false);
+		return (0);
 }
 
-int     ft_atoi(char *str)
+int	ft_atoi(char *str)
 {
-    int num;
-    int sign;
-    int i;
+	int i;
+	int num;
+	int sign;
 
-    i = num = 0;
-    sign = 1;
-
-    while (is_space(str[i]))
-        i++;
-    if (str[i] == 45 || str[i] == 43)
-    {
-        if (str[i] == 45)
-            sign = -1;
-        i++;
-    }
-
-    while (is_digit(str[i]))
-    {
-        num = (num * 10) + (str[i] - 48);
-        i++;
-    }
-
-    return (num * sign);
+	i = 0;
+	num = 0;
+	sign = 1;
+	while (ft_is_space(str[i]))
+		i++;
+	if (str[i] == 45 || str[i] == 43)
+	{
+		if (str[i] == 45)
+			sign = -1;
+		i++;
+	}
+	while (ft_is_digit(str[i]))
+	{
+		num = (num * 10) + (str[i] - 48);
+		i++;
+	}
+	return (num * sign);
 }
