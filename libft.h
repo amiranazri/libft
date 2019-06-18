45a669350ddcaa1f0f08111dd0b1e4d1a560b179
Nazri AMIRA <anazri@c4r18s2.wethinkcode.co.za>
2019-06-18 13:24:58 +0200

    Final

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: anazri <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/12 15:49:09 by anazri            #+#    #+#             */
-/*   Updated: 2019/06/12 16:09:21 by anazri           ###   ########.fr       */
+/*   Updated: 2019/06/18 13:19:20 by anazri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@
 # include <stddef.h>
 # include <string.h>
 
-typedef enum e_bool
+typedef enum	e_bool
 {
 	false, true
-}	t_bool;
+}				t_bool;
 
 int		ft_atoi(char *str);
 void	ft_bzero(void *s, size_t n);
@@ -43,28 +43,28 @@ void	ft_putchar(char c);
 int		ft_putchar_fd(int c, int fd);
 void	ft_putendl(char const *s);
 void	ft_putendl_fd(char const *s, int fd);
-void 	ft_putnbr(int n);
-void 	ft_putnbr_fd(int n, int fd);
+void	ft_putnbr(int n);
+void	ft_putnbr_fd(int n, int fd);
 void	ft_putstr(char const *s);
 void	ft_putstr_fd(char const *s, int fd);
 char	*ft_strcat(char *s1, const char *s2);
 char	*ft_strchr(const char *s, int c);
 void	ft_strclr(char *s);
-int     ft_strcmp(const char *s1, const char *s2);
+int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strcpy(char *dest, const char *src);
 void	ft_strdel(char **as);
 char	*ft_strdup(const char *s1);
-void    *ft_striteri(char *s, void (*f) (unsigned int, char *));
+void	*ft_striteri(char *s, void (*f) (unsigned int, char *));
 char	*ft_strndup(const char *s1, size_t n);
 int		ft_strequ(char const *s1, char const *s2);
 void	ft_striter(char *s, void (*f)(char *));
-char    *ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char *str);
-char    *ft_strmap(char const *s, char (*f)(char));
-char    *ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char	*ft_strmap(char const *s, char (*f)(char));
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-char	*ft_strncpy(char *dst, const char *src, size_t len); 
+char	*ft_strncpy(char *dst, const char *src, size_t len);
 int		ft_strnequ(char const *s1, char const *s2, size_t n);
 char	*ft_strnew(size_t size);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
@@ -73,7 +73,7 @@ char	**ft_strsplit(char const *s, char c);
 char	*ft_strstr(const char *haystack, const char *needle);
 char	*ft_strsub(char const *s, unsigned int start, size_t len);
 char	*ft_strlowcase(char *str);
-char 	*ft_strupcase(char *str);
+char	*ft_strupcase(char *str);
 size_t	ft_token_count(char *str, int c);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
