46836fde3aee6ca280782cab224cd2efafdda2e2
interns <interns@internss-iMac.local>
2019-06-18 12:53:04 +0200

    Final

--- libft.h
+++ libft.h
@@ -30,7 +30,7 @@ int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
-char	*ft_itoa(int n);
+char	*ft_itoa(intmax_t n);
 void	*ft_memalloc(size_t size);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
